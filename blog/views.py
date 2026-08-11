65bad3be28ddc820bdb241522056c9b1502b6a64
amir barati <amir5133000@gmail.com>
2026-08-11 11:56:49 +0330

    Add single blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import BlogPost
 
 def post_list(request):
     posts = BlogPost.objects.all()
-    return render(request, "blog/post_list.html", {'posts':posts})
+    return render(request, "blog/post_list.html", {"posts":posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(
+        BlogPost,
+        slug=slug,
+        status="published",
+    )
+    return render(request, "blog/post_detail.html", {"post":post})
