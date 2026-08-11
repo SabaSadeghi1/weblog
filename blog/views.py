37c2f2c645f1b897031caac47aaf75167aedce38
Saba Sadeghi <sabasadeghi224@gmail.com>
2026-08-11 10:21:38 +0330

    Update migrations for rewritten models

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import BlogPost
 
-# Create your views here.
+def post_list(request):
+    posts = BlogPost.objects.all()
+    return render(request, "blog/post_list.html", {'posts':posts})
--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('blog/', views.post_list, name='post_list'),
 ]
 
 
