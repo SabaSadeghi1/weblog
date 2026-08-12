b81745e77cafd1df42c77e2c32888e66b0cfc0df
amir barati <amir5133000@gmail.com>
2026-08-12 13:29:21 +0330

    Add CKEditor to profile bio and site descriptions

--- core/models.py
+++ core/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.conf import settings
+from django_ckeditor_5.fields import CKEditor5Field
 
 class SiteSettings(models.Model):
     singleton_key = models.CharField(max_length=20, default='global', unique=True)
     site_name = models.CharField(max_length=100)
-    site_description = models.TextField(null=True, blank=True)
+    site_description = CKEditor5Field(config_name="extends",null=True, blank=True)
     default_seo_title = models.CharField(max_length=200, null=True, blank=True)
-    default_seo_description = models.TextField(null=True, blank=True)
+    default_seo_description =CKEditor5Field(config_name="extends",null=True, blank=True)
 #    registration_enabled = models.BooleanField(default=True)
     comments_approval = models.BooleanField(default=False)
     updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
