ab22b15496ad46981f2b8d3091370432afc45918
Saba Sadeghi <sabasadeghi224@gmail.com>
2026-08-12 13:31:59 +0330

    Update blog post list

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
-# Create your views here.
+def user_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:landing')
+    else:
+       form = RegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-#from blog import views
+from blog import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("posts/", include("blog.urls")),
+    # path('blog/', views.post_list, name='post_list')
     path("", include("core.urls")),
+    path('register/', accounts_views.user_register, name='register'),
 ]
 
 if settings.DEBUG:
